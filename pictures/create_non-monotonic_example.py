0520f62a7d0a749913e443f8ad2b5d66baf819f1
SeverinoDaDalt <dadaltseverino@gmail.com>
2025-09-11 16:35:42 +0200

    Improve half-graph and k-tree pictures, and fix punctuation in eqautions.

--- pictures/create_non-monotonic_example.py
+++ pictures/create_non-monotonic_example.py
@@ -8,7 +8,7 @@ NODE_SIZE=0.3
 CAPTION=(f"Example of the $\epsilon$-excellence property not being monotonic. \n"
          f"\\emph{{On the left}}, a bipartite graph with two independent sets $A$ and $B$. \n"
          f"A simple exhaustive check shows that $A$ is $\\frac{{1}}{{5}}$-excellent. \n"
-         f"On the other, raising the $\epsilon$-value up to $\\frac{{2}}{{5}}$ introduces a new \n"
+         f"On the other hand, raising the $\epsilon$-value up to $\\frac{{2}}{{5}}$ introduces a new \n"
          f"$\\frac{{2}}{{5}}$-good set $B$ witnessing that $A$ is not excellent, as half of the vertices of $A$ have one \n"
          f"truth value, and half the other. \n"
          f"\\emph{{On the right}} is the corresponding bi-adjacency matrix.")
